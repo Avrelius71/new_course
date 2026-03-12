cae17a91a3f6d5ad55ebe612cb6a7f85803d18ce
Avrelius71 <artem.dryamin_96@mail.ru>
2026-03-13 00:46:31 +0300

    TASK-495

---
+++ test5.py
@@ -0,0 +1,17 @@
+import requests
+import time
+
+
+response1 = requests.get('https://playground.learnqa.ru/ajax/api/longtime_job')
+token_values = response1.json()['token']
+token = {'token': token_values}
+response2 = requests.get('https://playground.learnqa.ru/ajax/api/longtime_job', params=token)
+print(response2.text)
+if response2.json()['status'] == 'Job is NOT ready':
+    print('Задача еще не выполнена, статус корректный')
+print('Ждем 20 сек')
+time.sleep(20)
+response3 = requests.get('https://playground.learnqa.ru/ajax/api/longtime_job', params=token)
+print(response3.text)
+if 'result' in response3.json() and response3.json()['status'] == 'Job is ready':
+    print('поле result присутствует, статус корректный')
