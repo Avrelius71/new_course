3d32da621558df4c7495e444056f46553ff05575
Avrelius71 <artem.dryamin_96@mail.ru>
2026-03-16 22:20:37 +0300

    TASK-494 update

--- test4.py
+++ test4.py
@@ -8,21 +8,28 @@ data = [
 
 response1 = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type')
 response2 = requests.head('https://playground.learnqa.ru/ajax/api/compare_query_type')
-response3 = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type', params='GET')
+response3 = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type', params={'method': 'GET'})
 
 print('респонс1')
 print(response1.text)
 print(response1.status_code)
 print('респонс2')
-print(response2.text)
+print(response2.headers)
 print(response2.status_code)
 print('респонс3')
 print(response3.text)
 print(response3.status_code)
 
-for i in data:
-    response4 = requests.post('https://playground.learnqa.ru/ajax/api/compare_query_type', data=i)
-    print(i)
-    print(response4.text)
-    print(response4.status_code)
+for param_value in data:
+    for http_method in methods:
+
+        if http_method == 'GET':
+            response4 = requests.request(http_method, 'https://playground.learnqa.ru/ajax/api/compare_query_type', params=param_value)
+        else:
+            response4 = requests.request(http_method, 'https://playground.learnqa.ru/ajax/api/compare_query_type', data=param_value)
+
+        print(f"Метод: {http_method}, Параметр: {param_value['method']}")
+        print(f"Статус: {response4.status_code}")
+        print(f"Ответ: {response4.text.strip()}")
+        print('-' * 60)
 
